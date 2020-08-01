18ed80ff8c2eddd8aa729ed897487615d80d3def
tibwere <tib.sim.98@gmail.com>
2020-08-01 19:49:45 +0200

    Aggiornata la gestione della configurazione di default e spostata la funzione del parsing della command line

--- include/common.h
+++ include/common.h
@@ -10,8 +10,6 @@ enum app_usages {
         ERROR
 };
 
-enum app_usages parse_cmd(int argc, char **argv, struct gbn_config *conf, int *port);
-void default_gbn_configuration(struct gbn_config* cfg);
 void error_handler(const char *message);
 
 #endif
--- src/server.c
+++ src/server.c
@@ -3,13 +3,61 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "gbn.h" 
 #include "common.h"
+
+#define DEFAULT_PORT 2929
 
 int sockfd;
 unsigned short int port;
 socklen_t len;
 struct sockaddr_in addr;
+
+extern const struct gbn_config default_config;
+
+extern char *optarg;
+extern int opterr;
+
+enum app_usages parse_cmd(int argc, char **argv, struct gbn_config *conf, int *port)
+{
+        int opt;
+
+        struct option long_options[] = {
+                {"port",        required_argument,      0, 'p'},
+                {"windowsize",  required_argument,      0, 'N'},
+                {"rto",         required_argument,      0, 't'},
+                {"probability", required_argument,      0, 'P'},
+                {"help",        no_argument,            0, 'h'},
+                {"version",     no_argument,            0, 'v'},
+                {0,             0,                      0, 0}
+        };
+
+        while ((opt = getopt_long(argc, argv, "p:N:t:P:hv", long_options, NULL)) != -1) {
+                switch(opt) {
+                        case 'p':
+                                *port = strtol(optarg, NULL, 10);
+                                break;
+                        case 'N':
+                                conf->N = strtol(optarg, NULL, 10);
+                                break;
+                        case 't':
+                                conf->rto_msec = strtol(optarg, NULL, 10);
+                                break;
+                        case 'P':
+                                conf->probability = strtol(optarg, NULL, 10) / 100;
+                                break;
+                        case 'h':
+                                return (argc != 2) ? ERROR : HELP;
+                        case 'v':
+                                return (argc != 2) ? ERROR : VERSION;
+                }
+        }
+
+        return STANDARD;
+}
 
 int setup_server()
 {
@@ -46,14 +94,15 @@ int main(int argc, char **argv)
         int port;
         enum app_usages modality;
 
-        default_gbn_configuration(&config);
+        memcpy(&config, &default_config, sizeof(config));
+        port = DEFAULT_PORT;
 
         modality = parse_cmd(argc, argv, &config, &port);
 
         switch(modality) {
                 case STANDARD: 
-                        printf("Configs:\n\tN: %u\n\tt: %lu\n\tP: %f\n\tp: %u\n", 
-                                config.N, config.rto_msec, config.probability, port);
+                        printf("Configs:\n\tN: %u\n\trcvtimeout: %lu\n\tprobability: %f\n\tport: %u\n\tadapitve: %s\n\n", 
+                                config.N, config.rto_msec, config.probability, port, (config.is_adaptive) ? "true" : "false");
                         break;
                 default:
                         printf("Not yet implemented\n");             
